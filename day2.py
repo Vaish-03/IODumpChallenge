d9d55399807834d3cf7b9d4126018aa19454ea70
Vaish-03 <115860627+Vaish-03@users.noreply.github.com>
2023-03-25 13:56:55 +0530

    Add files via upload
    Program to find a area of traingle ,square, rectangle

---
+++ day2.py
@@ -0,0 +1,22 @@
+#program to find the area of traingle,rectanle,square
+
+#area of traingle
+h=int(input("Enter the height of the triangle"))
+b=int(input("Enter the breadth of the triangle"))
+tarea=float((0.5)*(b)*(h))
+print(" AREA OF TRIANGLE")
+print("The area of Triangle is {0}",tarea)
+
+#area of square
+l=int(input("Enter the length of the square"))
+sarea=float(l*l)
+print(" AREA OF SQUARE")
+print("The area of Square is {0}",sarea)
+
+#area of rectangle
+w=int(input("Enter the width of the rectangle"))
+h=int(input("Enter the height of the rectangle"))
+rarea=float(w*h)
+
+print(" AREA OF RECTANGLE")
+print("The area of rectangle is {0}",rarea)
